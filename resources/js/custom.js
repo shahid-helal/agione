587469f496fa4906fda09aa6ae03ad87f45a7eba
Shahidul Islam <sihelal72@gmail.com>
2021-04-01 22:10:10 +0600

    music slider fixed

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -67,6 +67,71 @@
                 return false;
             });
         },
+        
+        /* ============================================================ */
+        /* Music Slider
+        /* ============================================================ */
+        musicSlider: function() {
+            $('.music-slider').slick({
+                autoplay: false,                
+                dots: false,
+                centerMode: true,
+                focusOnSelect: true,
+                arrows: false,
+                centerPadding: '0',
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                responsive: [   
+                    {
+                        breakpoint: 992,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },                 
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                            slidesToScroll: 1,
+                            centerMode: true,
+                        }
+                    }
+                ]
+            });
+
+            $(".modal-trigger").on("click",function(e){
+                e.preventDefault();
+                if($(this).attr("data-iframe") !== undefined) {
+                    $("#modalframe").attr("src", $(this).attr("data-iframe"));
+                }
+                return true;
+            });
+            function triggerModal(selector, actionSelector) {
+                var triggerModal = $(selector);
+                triggerModal.on("click", function() {
+                    $(actionSelector).addClass('open');
+                });
+                
+        
+                $(document).on('click', function(e) {
+                    var selectorType = $(actionSelector).add(triggerModal);
+                    if (selectorType.is(e.target) !== true && selectorType.has(e.target).length === 0) {
+                        $(actionSelector).removeClass("open");
+                    }          
+                });
+        
+            };
+            triggerModal('.modal-trigger', '.modal-track');
+
+        },
 
         /* ============================================================ */
         /* Background Image
@@ -126,6 +191,7 @@
 			agione.preloader();
 			agione.mobile_menu();
 			agione.sticky_menu();
+			agione.musicSlider();
 			agione.background_image();
 		}
 
